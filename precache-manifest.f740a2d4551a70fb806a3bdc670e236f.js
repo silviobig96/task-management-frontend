364f79053b08564cdabdf071c16b105c330750f3
silviobig96 <silviobig96@users.noreply.github.com>
2021-05-27 22:54:54 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.f740a2d4551a70fb806a3bdc670e236f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d23486a816776cfef08b8edf1205fb4",
+    "url": "https://silviobig96.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ad1ef6a4a0e68551cec4",
+    "url": "https://silviobig96.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "7eb64fddc2111e2e405b",
+    "url": "https://silviobig96.github.io/task-management-frontend/static/js/2.8a10ba65.chunk.js"
+  },
+  {
+    "revision": "ad1ef6a4a0e68551cec4",
+    "url": "https://silviobig96.github.io/task-management-frontend/static/js/main.8baf69f9.chunk.js"
+  },
+  {
+    "revision": "a122614bdf261d895a88",
+    "url": "https://silviobig96.github.io/task-management-frontend/static/js/runtime~main.322e82a7.js"
+  }
+]);
